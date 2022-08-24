c6ed35f1020ce1c5be6fb7a59d38d4ad89eeeab6
Aaryan R S <82698176+Aaryan-R-S@users.noreply.github.com>
2022-08-24 13:50:19 +0530

    Revert "Fixed implementation contract verification issues and added support f…"

--- deploy/02-deploy-box2.js
+++ deploy/02-deploy-box2.js
@@ -1,7 +1,6 @@
 const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
 
 const { network } = require("hardhat")
-const { verify } = require("../helper-functions")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
@@ -12,10 +11,10 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         : VERIFICATION_BLOCK_CONFIRMATIONS
 
     log("----------------------------------------------------")
-    const arguments = []
+
     const box = await deploy("BoxV2", {
         from: deployer,
-        args: arguments,
+        args: [],
         log: true,
         waitConfirmations: waitBlockConfirmations,
     })
